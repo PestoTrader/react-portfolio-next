bbd000a616c1f6704fb62ce154e9b6e76679dbb2
Theo S <theo.scottt@gmail.com>
2022-10-23 16:52:11 +0200

    new commit

---
+++ public/Components/ThoughtPlanner.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+
+import { Route, Link, useRouteMatch } from 'react-router-dom';
+
+import { AddThoughtForm } from './AddThoughtForm';
+import { Thought } from './Thought';
+import { generateId, getNewExpirationTime } from './Utilities';
+
+
+export default function ThoughtPlanner () {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing thoughts.',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
+  const addThought = (thought) => {
+    setThoughts(prev => [thought, ...prev])
+  }
+
+  const removeThought = (thoughtIdToRemove) => {
+    setThoughts(prev => prev.filter(thought => (thought.id !== thoughtIdToRemove)))
+  }
+
+  return (
+    <div className="Appi">
+     
+      <main>
+        <AddThoughtForm  addThought={addThought}/>
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought} removeThought={removeThought}/>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
