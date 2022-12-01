ec288f0002afd4a2b51e7c8fea6eb104b10ed542
Theo S <theo.scottt@gmail.com>
2022-12-01 11:18:03 +0100

    spelling correction

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ export default function Home() {
           <div  className=" text-center font-montserrat  p-10  my-10 rounded-lg">
           <p  className="text-gray-600 p-10">Full-Mint-Project</p>
          <RoboPunksNFT className="md:h-96 md:w-96"/>
-         <p className=" font-montserrat text-gray-600 font-light text-lg max-w-5xl text-center dark:text-white" > This project combines a Solidity contract with Ethereum using the Goerli testnetwork to make a smart contract that sits inside a React app. The connect button will connect your MetaMask and from there you can mint, although I have disabled this feature as I don't have an NFT to be minted. The contract address is the follow: 0x33593636c4411e2D00a51B97deE2B5c0B6A606eB which can be consulted on etherscan(goerli).</p>
+         <p className=" font-montserrat text-gray-600 font-light text-lg max-w-5xl text-center dark:text-white" > This project combines a Solidity contract with Ethereum using the Goerli testnetwork to make a smart contract that sits inside a React app. The connect button will connect your MetaMask and from there you can mint, although I have disabled this feature as I don't have an NFT to be minted. The contract address is the following: 0x33593636c4411e2D00a51B97deE2B5c0B6A606eB which can be consulted on etherscan(goerli).</p>
          <a type="button" href="https://full-mint-website-ten.vercel.app/" target="_blank" rel="noreferrer" className="text-white bg-gradient-to-r from-cyan-500 to-teal-500 rounded-lg text-sm px-5 py-2.5 text-center font-montserrat mr-2 mb-2">VISIT MINT SITE</a>
          </div>
   
